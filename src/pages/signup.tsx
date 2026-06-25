efbe0e3e4bcedd06698e44282a80d1b3387c6c3f
itzmeputter <ac55523@assumption.ac.th>
2026-06-25 08:24:50 +0700

    Import useSignup hook in signup.tsx

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useLocation, Link } from "wouter";
-
+import { useSignup } from "@/hooks/use-signup";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
